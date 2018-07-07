28ddbb52c2c6892b89b3eb8c65b545b5e0630a21
Eric Moyer <eric@lemoncrab.com>
2018-07-07 07:30:36 -0700

    make Netlist a class

--- netdiff.py
+++ netdiff.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Standard library
-import pprint
+from textwrap import indent
 
 # Library
 from docopt import docopt
@@ -21,37 +21,36 @@ __version__ = '0.1.0'
 
 def main():
     arguments = docopt(USAGE, version=__version__)
-    nets = get_nets(arguments['<file1>'])
-    # pp = pprint.PrettyPrinter(indent=3)
-    # pp.pprint(nets)
-    for net in nets:
-        print(net)
+    # nets = get_nets(arguments['<file1>'])
+    # for net in nets:
+    #     print(net)
+    netlist_left = Netlist(arguments['<file1>'])
+    netlist_left.dump()
 
-
-def get_nets(filname):
-    with open(filname) as f:
-        content = f.readlines()
-    nets = []
-    net = None
-    for line in content:
-        line = line.rstrip('\n')
-        if line.startswith('*SIGNAL*'):
-            if net:
-                nets.append(net)
-            # net = {'name': line.replace('*SIGNAL* ', ''), 'nodes':[]}
-            net_name = line.replace('*SIGNAL* ', '')
-            net = Net(net_name)
-            continue
-        if net:
-            if line.startswith('*'):
-                nets.append(net)
-                net = None
-                continue
-            else:
-                # net['nodes'] += line.strip().split(' ')
-                nodes = line.strip().split(' ')
-                net.add_nodes(nodes)
-    return nets
+# def get_nets(filname):
+#     with open(filname) as f:
+#         content = f.readlines()
+#     nets = []
+#     net = None
+#     for line in content:
+#         line = line.rstrip('\n')
+#         if line.startswith('*SIGNAL*'):
+#             if net:
+#                 nets.append(net)
+#             # net = {'name': line.replace('*SIGNAL* ', ''), 'nodes':[]}
+#             net_name = line.replace('*SIGNAL* ', '')
+#             net = Net(net_name)
+#             continue
+#         if net:
+#             if line.startswith('*'):
+#                 nets.append(net)
+#                 net = None
+#                 continue
+#             else:
+#                 # net['nodes'] += line.strip().split(' ')
+#                 nodes = line.strip().split(' ')
+#                 net.add_nodes(nodes)
+#     return nets
 
 class Net:
     def __init__(self, name, nodes=None):
@@ -66,6 +65,36 @@ class Net:
 
     def __str__(self):
         return f'{self.name}: {", ".join(self.nodes)}'
+
+class Netlist:
+    def __init__(self, filename):
+        self.filename = filename
+        self.nets = []
+        with open(filename) as f:
+            content = f.readlines()
+        net = None
+        for line in content:
+            line = line.rstrip('\n')
+            if line.startswith('*SIGNAL*'):
+                if net:
+                    self.nets.append(net)
+                net_name = line.replace('*SIGNAL* ', '')
+                net = Net(net_name)
+                continue
+            if net:
+                if line.startswith('*'):
+                    self.nets.append(net)
+                    net = None
+                    continue
+                else:
+                    nodes = line.strip().split(' ')
+                    net.add_nodes(nodes)
+        
+    def dump(self):
+        print(self.filename)
+        for net in self.nets:
+            print(net)
+
 
 if __name__ == '__main__':
     main()
