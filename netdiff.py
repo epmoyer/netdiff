3d193859cbacb41e9701bdb09a516d4211480b59
Eric Moyer <eric@lemoncrab.com>
2018-07-07 07:25:42 -0700

    make Net a class

--- netdiff.py
+++ netdiff.py
@@ -22,8 +22,10 @@ __version__ = '0.1.0'
 def main():
     arguments = docopt(USAGE, version=__version__)
     nets = get_nets(arguments['<file1>'])
-    pp = pprint.PrettyPrinter(indent=3)
-    pp.pprint(nets)
+    # pp = pprint.PrettyPrinter(indent=3)
+    # pp.pprint(nets)
+    for net in nets:
+        print(net)
 
 
 def get_nets(filname):
@@ -31,13 +33,14 @@ def get_nets(filname):
         content = f.readlines()
     nets = []
     net = None
-    in_signal = False
     for line in content:
         line = line.rstrip('\n')
         if line.startswith('*SIGNAL*'):
             if net:
                 nets.append(net)
-            net = {'name': line.replace('*SIGNAL* ', ''), 'nodes':[]}
+            # net = {'name': line.replace('*SIGNAL* ', ''), 'nodes':[]}
+            net_name = line.replace('*SIGNAL* ', '')
+            net = Net(net_name)
             continue
         if net:
             if line.startswith('*'):
@@ -45,8 +48,24 @@ def get_nets(filname):
                 net = None
                 continue
             else:
-                net['nodes'] += line.strip().split(' ')
+                # net['nodes'] += line.strip().split(' ')
+                nodes = line.strip().split(' ')
+                net.add_nodes(nodes)
     return nets
+
+class Net:
+    def __init__(self, name, nodes=None):
+        self.name = name
+        self.nodes = [] if nodes is None else nodes
+
+    def add_nodes(self, nodes):
+        self.nodes += nodes
+
+    def __repr__(self):
+        return f"Net('{self.name}', {self.nodes})"
+
+    def __str__(self):
+        return f'{self.name}: {", ".join(self.nodes)}'
 
 if __name__ == '__main__':
     main()
